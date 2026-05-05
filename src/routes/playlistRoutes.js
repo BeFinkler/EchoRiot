21405106dedea5ce82dc973c91519ba80e39a76e
BeFinkler <finklerbernardo@gmail.com>
2026-05-05 12:08:52 -0300

    Routees
    Rotas de autentificação e de playlists.

---
+++ src/routes/playlistRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const auth = require('../middlewares/authMiddleware');
+const ctrl = require('../controllers/playlistController');
+
+router.post('/', auth, ctrl.create);
+router.get('/', ctrl.getAll);
+router.post('/:id/songs', auth, ctrl.addSong);
+router.post('/:id/like', auth, ctrl.like);
+
+module.exports = router;
