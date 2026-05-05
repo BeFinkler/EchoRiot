1bfe95ba3167e2e7b4a39eeb1d87a7fe95ec1fb6
BeFinkler <finklerbernardo@gmail.com>
2026-05-04 22:10:13 -0300

    Creation
    Primeira versão, apenas para criação das pastas e arquivos

---
+++ src/app.js
@@ -0,0 +1,24 @@
+﻿require('dotenv').config();
+
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({ status: 'ok', message: 'EchoRiot API is running' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
